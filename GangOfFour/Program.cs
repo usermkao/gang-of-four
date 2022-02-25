f69fef7dfe8b50d0651221efe2ed057d071b8039
BHG\mkao <mondulkao@hotmail.com>
2022-02-25 16:40:55 -0500

    initial commit

---
+++ GangOfFour/Program.cs
@@ -0,0 +1,24 @@
+﻿using DoFactory.GangOfFour.Abstract.RealWorld;
+using DoFactory.GangOfFour.Builder.RealWorld;
+
+namespace GangOfFour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAbstractFactory();
+            RunBuilder();
+        }
+
+        static void RunAbstractFactory()
+        {
+            AbstractFactory.Start();
+        }
+
+        static void RunBuilder()
+        {
+            Builder.Start();
+        }
+    }
+}
